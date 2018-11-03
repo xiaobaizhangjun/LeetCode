f5572a932954677e034a6cdbd99a318573aae4a7
zhangjun <zhangjunx6497@163.com>
2018-11-03 22:07:27 +0800

    添加两道leetcode算法

---
+++ LeetCode/Calculate.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace LeetCode
+{
+    //实现一个基本的计算器来计算一个简单的字符串表达式的值。
+
+    //字符串表达式仅包含非负整数，+， - ，*，/ 四种运算符和空格  。 整数除法仅保留整数部分。
+
+    //示例 1:
+
+    //输入: "3+2*2"
+    //输出: 7
+    //示例 2:
+
+    //输入: " 3/2 "
+    //输出: 1
+    //示例 3:
+
+    //输入: " 3+5 / 2 "
+    //输出: 5
+    //说明：
+
+    //你可以假设所给定的表达式都是有效的。
+    //请不要使用内置的库函数 eval
+    public class Calculate
+    {
+        public int Calculates(string express)
+        {
+            int result = 0;
+            express = express.Replace(" ", "");
+            string[] s1 = express.Split(new char[2] { '+', '-' });
+            foreach (string ex in s1)
+            {
+                string[] s2 = ex.Split(new char[2] { '*', '/' });
+                char[] ops1 = Regex.Replace(ex, @"[0-9]", "").ToCharArray();
+                string num = Operation(s2, ops1).ToString();
+                express = express.Replace(ex, num);
+            }
+            string[] s3 = express.Split(new char[2] { '+', '-' });
+            char[] ops2 = Regex.Replace(express, @"[0-9]", "").ToCharArray();
+            result = Operation(s3, ops2);
+            return result;
+        }
+        private static int Operation(string[] nums, char[] ops)
+        {
+            int num = Int32.Parse(nums[0]);
+            for (int i = 1; i < nums.Length; i++)
+            {
+                switch (ops[i - 1])
+                {
+                    case '*':
+                        num = num * Int32.Parse(nums[i]);
+                        break;
+                    case '/':
+                        num = num / Int32.Parse(nums[i]);
+                        break;
+                    case '+':
+                        num = num + Int32.Parse(nums[i]);
+                        break;
+                    case '-':
+                        num = num - Int32.Parse(nums[i]);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return num;
+        }
+    }
+
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace LeetCode
 {
-    class Program
+    //    给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
+
+    //你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
+
+    //示例:
+
+    //给定 nums = [2, 7, 11, 15], target = 9
+
+    //因为 nums[0] + nums[1] = 2 + 7 = 9
+    //所以返回[0, 1]
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] nums = new int[] { 1, 25, 30, 20 };
+            Program program = new Program();
+            int[] result = program.TwoSum(nums, 21);
+            foreach (var num in result)
+            {
+                Console.Write("{0} ", num);
+            }
+            Console.ReadKey();
+        }
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[j] == target - nums[i])
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return new int[] { };
         }
     }
 }
